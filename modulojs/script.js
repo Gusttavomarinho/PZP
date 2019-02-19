9f2b2de2e6c5dfada30d05426c4c4dfd90e291b1
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-02-19 20:37:43 -0300

    aula de como criar função em JS

--- modulojs/script.js
+++ modulojs/script.js
@@ -8,3 +8,11 @@ linhas
 */
 
 var nome = "Gustavo";
+//criando uma função no JS
+function fazerAcao(nome,idade){
+	var area = document.getElementById("area");
+	var texto = prompt("Qual o seu sobrenome?");
+
+	area.innerHTML = nome+" "+texto+" Tem "+idade+" Anos ";
+}
+
