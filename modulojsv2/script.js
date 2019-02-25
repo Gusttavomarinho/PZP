96f0c283176265f281cb50942870b0a7f2a63985
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-02-25 19:16:39 -0300

    Aula sobre eventos mouse JS

---
+++ modulojsv2/script.js
@@ -0,0 +1,37 @@
+function apertouMouse() {
+	console.log("APERTOU O MOUSE AAAAE!");
+}
+
+function soltouMouse(){
+	console.log("SOLTOU O MOUSE :( ");
+}
+
+function mouseEmCima(){
+	console.log("PASSOU O MOUSE EM CIMA DO BOTAO!!!");
+}
+
+function mouseSaioDeCima(){
+	console.log("MOUSE SAIO DE  CIMA DO BOTAO!!!");
+}
+
+function MoveMouse(){
+	console.log("MOUSE FOI MOVIDO");
+}
+
+function clicou(){
+	console.log("CLICOU NO BOTAO");
+}
+
+function botaoDireito(){
+	console.log("BOTAO DIREITO");
+
+	return false;
+}
+
+function cliqueDuplo(){
+	console.log("Clicou duas vezes");
+}
+
+function mudarCor(){
+	document.getElementById('body').style.backgroundColor = 'black';
+}
