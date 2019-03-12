7f20be77de190050a2c95a4f9793bdc191c1fcc1
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-11 23:04:34 -0300

    iniciando com alguns loop em php

--- PHPintermediario/index.php
+++ PHPintermediario/index.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+//trabalhando com while
 $x = 1;
 
 while ($x <= 10) {
@@ -9,5 +10,34 @@ while ($x <= 10) {
 	$x++;
 }
 
-
 // $x++ eu mostro ou faço algo para depois incrementar e ++$x incrementa para depois fazer algo
+
+
+//trbalhando com for
+
+for($x = 0; $x <= 10; $x++){
+
+	echo "{for}		- 	X é igual a ".$x."<br/>";
+}
+
+echo "<br/>";
+
+//trabalhando com foreach
+
+$nomes =  array(
+	array("nome"=>"Bonieky","idade"=>90),
+	array("nome"=>"Gustavo","idade"=>22),
+	array("nome"=>"Fulano","idade"=>19),
+	array("nome"=>"Ze","idade"=>125),
+	array("nome"=>"Cicrano","idade"=>18),
+	array("nome"=>"Maria","idade"=>20),
+	array("nome"=>"Joaquina","idade"=>21)
+	
+);
+
+foreach ($nomes as  $aluno) {
+	# code...
+	echo "Aluno: ".$aluno["nome"]." - Idade: ".$aluno["idade"]."<br/>";
+}
+
+?>
