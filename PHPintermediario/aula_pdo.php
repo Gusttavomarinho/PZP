7dd6ead81a612429d4f42ce57fae7cdd605f9c42
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-18 22:50:35 -0300

    aula01 de PDO com PHP

---
+++ PHPintermediario/aula_pdo.php
@@ -0,0 +1,13 @@
+<?php
+//define qual o sgbd , depois o nome do banco de dados , ip do servidor
+$dsn = "mysql:dbname=pzp_blog;host=localhost";
+$dbuser = "root";
+$dbpass = "";
+
+try {
+	$pdo = new PDO($dsn,$dbuser,$dbpass);
+	echo "conexão estabelecida com sucesso!";
+} catch(PDOException $e){
+	echo "Falhou: ".$e->getMessage();
+}
+?>
