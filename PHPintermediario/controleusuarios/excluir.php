1ae1e725bf5790ce0730fa66f508087434250fe1
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-20 20:04:53 -0300

    aula - criando estrutura de deletar usuario PHP

--- PHPintermediario/controleusuarios/excluir.php
+++ PHPintermediario/controleusuarios/excluir.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gustt
- * Date: 20/03/2019
- * Time: 19:24
- */
+require 'config.php';
+if(isset($_GET['id']) && empty($_GET['id']) == false){
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM usuarios WHERE id = :id";
+    $sql = $pdo->prepare($sql);
+    $sql->bindValue(':id',$id);
+    $sql->execute();
+
+    header("Location: index.php");
+
+}else {
+    header("Location: index.php");
+}
+?>
