db91092621737c15afaa04f964d34e5e90e77688
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-20 19:54:34 -0300

    aula - criando estrutura de add usuario PHP

--- PHPintermediario/controleusuarios/adcionar.php
+++ PHPintermediario/controleusuarios/adcionar.php
@@ -1,3 +1,21 @@
+<?php
+require 'config.php';
+if(isset($_POST['nome']) && empty($_POST['nome']) == false ){
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = md5($_POST['senha']);
+
+    $sql = "INSERT INTO usuarios SET nome = :nome , email = :email , senha = :senha";
+    $sql = $pdo->prepare($sql);
+    $sql->bindValue(':nome',$nome);
+    $sql->bindValue(':email',$email);
+    $sql->bindValue(':senha',$senha);
+    $sql->execute();
+
+    header("Location: index.php");
+}
+?>
 <form method="POST">
     Nome:<br/>
     <input type="text" name="nome" placeholder="Digite seu nome"/><br/><br/>
