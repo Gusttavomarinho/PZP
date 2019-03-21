c618ee9e1c4f8cc613404cd7c3675bb10711f2cd
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-20 22:02:17 -0300

    comentando o codigo no final da aula

--- PHPintermediario/controleusuarios/adcionar.php
+++ PHPintermediario/controleusuarios/adcionar.php
@@ -1,18 +1,19 @@
 <?php
 require 'config.php';
+//verificando se o usuario enviou o nome ou se deixou em branco
 if(isset($_POST['nome']) && empty($_POST['nome']) == false ){
-
+    //coletandos os dados enviados
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = md5($_POST['senha']);
-
+    //realizando a consulta SQL
     $sql = "INSERT INTO usuarios SET nome = :nome , email = :email , senha = :senha";
     $sql = $pdo->prepare($sql);
     $sql->bindValue(':nome',$nome);
     $sql->bindValue(':email',$email);
     $sql->bindValue(':senha',$senha);
     $sql->execute();
-
+    //voltando para o index
     header("Location: index.php");
 }
 ?>
--- PHPintermediario/controleusuarios/excluir.php
+++ PHPintermediario/controleusuarios/excluir.php
@@ -1,17 +1,19 @@
 <?php
 require 'config.php';
+//verifica se foi passado o id ou nao
 if(isset($_GET['id']) && empty($_GET['id']) == false){
-
+    //coletando o id via get
     $id = $_GET['id'];
-
+    //realizando a query SQL e deletnado o usuario
     $sql = "DELETE FROM usuarios WHERE id = :id";
     $sql = $pdo->prepare($sql);
     $sql->bindValue(':id',$id);
     $sql->execute();
-
+    //voltando para o index
     header("Location: index.php");
 
 }else {
+    //se veio sem id ele cai no else e volta para o index
     header("Location: index.php");
 }
 ?>
