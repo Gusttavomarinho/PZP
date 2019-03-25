b5ab0ac084c6f3692e09d412cca41587c6a8d2d8
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-03-25 16:43:28 -0300

    trabalhando com upload de 1 arquivo no php

---
+++ PHPintermediario/upload/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gustavo Marinho
+ * Date: 25/03/2019
+ * Time: 16:23
+ */
+?>
+
+<form method="POST" enctype="multipart/form-data" action="recebedor.php">
+    <input type="file" name="arquivo" /><br/><br/>
+
+    <input type="submit" value="Enviar" />
+</form>
