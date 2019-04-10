421ee25824cabc83bf11d3ad1e0ed107154637a5
Gustavo Marinho <gusttavomarinho@gmail.com>
2019-04-10 20:21:41 -0300

    listando historico de movimentação

--- caixaeletronico/index.php
+++ caixaeletronico/index.php
@@ -12,6 +12,7 @@ if(isset($_SESSION['banco']) && empty($_SESSION['banco']) == false){
     $sql->bindValue(":id",$id);
     $sql->execute();
 
+    //pegando registro da conta
     if($sql->rowCount() > 0){
         $dado = $sql->fetch();
     }else{
@@ -36,5 +37,29 @@ if(isset($_SESSION['banco']) && empty($_SESSION['banco']) == false){
             Conta: <?php echo $dado['conta']; ?><br/>
             Saldo: <?php echo $dado['saldo']; ?><br/>
             <a href="sair.php">Sair</a>
+            <table border="0" width="100%" class="table">
+                <tr>
+                    <th scope="col">ID_CONTA</th>
+                    <th scope="col">TIPO</th>
+                    <th scope="col">VALOR</th>
+                    <th scope="col">DATA OPERAÇÃO</th>
+                </tr>
+                <?php
+                $sql = $pdo->prepare("SELECT * FROM historico WHERE id = :id");
+                $sql->bindValue(":id",$id);
+                $sql->execute();
+
+                if($sql->rowCount() > 0){
+                    foreach ($sql->fetchAll() as $movimentacao){
+                        echo '<tr>';
+                        echo '<td>'.$movimentacao['id_conta'].'</td>';
+                        echo '<td>'.$movimentacao['tipo'].'</td>';
+                        echo '<td>'.$movimentacao['valor'].'</td>';
+                        echo '<td>'.$movimentacao['data_operacao'].'</td>';
+                        echo '</tr>';
+                    }
+                }
+                ?>
+            </table>
     </body>
 </html>
