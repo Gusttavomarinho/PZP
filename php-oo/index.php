32454f2f861e68187eb9d41d8bb4eca28bbcdffa
Gustavo Marinho <gusttavomarinho@gmail.com>
2020-12-19 00:31:51 -0300

    fazendo uma revisao em php oo 2

--- php-oo/index.php
+++ php-oo/index.php
@@ -4,8 +4,16 @@
 //criando a class
 class Post {
     public $likes = 0;
-    public $comments = [];
+    public $comments = []; //isso sao as propiedades { caracteristicas }
     public $author;
+
+    public function increaseLike(){
+        $this->likes++;
+    }
+
+    public function decreaseLike(){
+        $this->likes--;
+    }
 }
 
 
@@ -13,9 +21,11 @@ class Post {
 
 $post1 = new Post();
 $post1->likes = 7;
+$post1->increaseLike();
 
 $post2 = new Post();
 $post2->likes = 10;
+$post2->decreaseLike();
 
 echo "POST 1: {$post1->likes} "."<br/>";
 
