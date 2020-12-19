6d477eb8eb3d40e84e9ee8a5b5f616413f9577c7
Gustavo Marinho <gusttavomarinho@gmail.com>
2020-12-19 00:17:16 -0300

    fazendo uma revisao em php oo

--- php-oo/index.php
+++ php-oo/index.php
@@ -1,3 +1,24 @@
 <?php
 
+//revisando o php OO
+//criando a class
+class Post {
+    public $likes = 0;
+    public $comments = [];
+    public $author;
+}
+
+
+//criando o objeto
+
+$post1 = new Post();
+$post1->likes = 7;
+
+$post2 = new Post();
+$post2->likes = 10;
+
+echo "POST 1: {$post1->likes} "."<br/>";
+
+echo "POST 2: {$post2->likes} "."<br/>";
+
   ?>
