bca8ea5d3b2607189c86c148a6e26fad2301f4a7
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-01-14 11:13:35 -0300

    revisando crud - update e delete  basico

---
+++ PHP1/crud/delete.php
@@ -0,0 +1,31 @@
+<?php 
+require 'config.php';
+$id = filter_input(INPUT_GET,'id');
+
+
+if ($id) {
+
+  //verificando se tem usuario cadastrado com este id
+
+  $sql = $pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
+  $sql->bindValue(':email',$id);
+  $sql->execute();
+  
+  if($sql->rowCount() === 0){
+      //deletando usuario no BD
+        $sql = $pdo->prepare("DELETE FROM usuarios WHERE id = :id");
+        $sql->bindValue(':id',$id);
+        $sql->execute();
+
+        header("Location: index.php");
+        exit;
+
+  } else {
+    header("Location: add.php");
+  }
+
+
+} else {
+  header("Location: add.php");
+  exit;
+}
