153274a4b292936ac6851322d49c17132025b842
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-01-14 09:31:32 -0300

    revisando crud - read  basico

--- PHP1/crud/add_action.php
+++ PHP1/crud/add_action.php
@@ -7,15 +7,28 @@ $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL); // JA FAZ UMA 
 
 if ($name && $email) {
 
-    $sql = $pdo->prepare("INSERT INTO usuarios (nome,email) VALUES (:name,:email) ");
-    $sql->bindValue(':name',$name);  //assiciação do valor (BindValue) , associação de varaivel (BindParam)
+    //verificando se ja tem o email cadastrado
+
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
     $sql->bindValue(':email',$email);
     $sql->execute();
+    
+    if($sql->rowCount() === 0){
+        //add usuario no BD
+          $sql = $pdo->prepare("INSERT INTO usuarios (nome,email) VALUES (:name,:email) ");
+          $sql->bindValue(':name',$name);  //assiciação do valor (BindValue) , associação de varaivel (BindParam)
+          $sql->bindValue(':email',$email);
+          $sql->execute();
 
-    header("Location: index.php");
-    exit;
+          header("Location: index.php");
+          exit;
+
+    } else {
+      header("Location: add.php");
+    }
+
 
 } else {
-  header("Location: add.php");
-  exit;
+    header("Location: add.php");
+    exit;
 }
--- PHP1/crud/index.php
+++ PHP1/crud/index.php
@@ -1,5 +1,17 @@
 <?php 
 require 'config.php';
+
+$lista = [];
+
+$sql = $pdo->query("SELECT * FROM usuarios");
+
+if($sql->rowCount() > 0){
+
+  $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+
 ?>
 <a href="add.php"> ADICIONAR USUARIO</a>
 <table border="1" width=100%>
@@ -9,4 +21,16 @@ require 'config.php';
       <th>EMAIL</th>
       <th>AÇÕES</th>
   </tr>
+  <?php foreach($lista as $usuario): ?>
+    <tr>
+      <td><?=$usuario['id']; ?></td>
+      <td><?=$usuario['nome']; ?></td>
+      <td><?=$usuario['email']; ?></td>
+      <td>
+          <a href="update.php?id=<?=$usuario['id']; ?>"> [ Editar ]</a>
+          <a href="delete.php?id=<?=$usuario['id']; ?>"> [ Excluir ]</a>
+      </td>
+  </tr>
+
+  <?php endforeach; ?>
 </table>
