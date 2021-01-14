dad970edc50656f763761e941e5f5f7449b5a628
Gustavo Marinho <gusttavomarinho@gmail.com>
2021-01-14 00:25:55 -0300

    usando o autoload do composer com minhas classes tb

--- PHP1/index.php
+++ PHP1/index.php
@@ -1,2 +1,31 @@
 <?php 
 require 'vendor/autoload.php';
+
+use \classes\matematica\Basica;
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('teste.log', Logger::WARNING));
+
+$m = new Basica();
+echo $m->somar(10,10);
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+
+
+/**
+ *  composer dump-autoload  // comando para gerar um novo autoload 
+ *  no composer.json crio isso aqui para usar/referenciar  meu autoload  
+ * 
+ * "autoload": {
+    *"psr-4": {
+     * "classes\\": "classes/"
+    *}
+    
+ */
